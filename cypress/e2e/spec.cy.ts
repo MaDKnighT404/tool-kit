86d7c70b1aba74e0711c4cf21ed3d234e4805d2b
George Koloidi <Battleaxe@bk.ru>
2023-05-25 20:31:41 +0200

    feat: add all tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -13,8 +13,12 @@ describe('App is normally rendered', () => {
 
   it('Greeting authorized user rendered', () => {
     cy.get('input[type="text"]').should('have.value', '');
-    cy.get('[data-cy="resultTitle"]').contains('Hello ');
-    cy.get('[data-cy="resultMessage"]').contains('This is your repositories:');
+    cy.get('[class*="result__title"]').contains('Hello ');
+    cy.get('[class*="result__message"]').contains('This is your repositories:');
+  });
+
+  it('User repositories rendered', () => {
+    cy.get('[class*="result__list"]').should('have.length.greaterThan', 0);
   });
 
   it('Pagination rendered', () => {
@@ -28,18 +32,105 @@ describe('Pagination work correctly', () => {
     cy.visit('http://localhost:5173/');
   });
 
-  it('Change active class after click on pagination list item', () => {
+  it('Change active class after click on pagination page', () => {
     cy.get('[class*="pagination__item"]')
       .eq(0)
       .invoke('attr', 'class')
       .should('contain', 'pagination__item_active');
     cy.get('[class*="pagination__item"]').eq(1).click();
-    cy.get('[class*="pagination__item"]')
-      .eq(0)
-      .should('not.contain', 'pagination__item_active');
+    cy.get('[class*="pagination__item"]').eq(0).should('not.contain', 'pagination__item_active');
     cy.get('[class*="pagination__item"]')
       .eq(1)
       .invoke('attr', 'class')
       .should('contain', 'pagination__item_active');
   });
+
+  it('Pagination page save after refresh page', () => {
+    cy.get('[class*="pagination__item"]')
+      .eq(0)
+      .invoke('attr', 'class')
+      .should('contain', 'pagination__item_active');
+    cy.get('[class*="pagination__item"]').eq(1).click();
+    cy.get('[class*="pagination__item"]').eq(0).should('not.contain', 'pagination__item_active');
+    cy.get('[class*="pagination__item"]')
+      .eq(1)
+      .invoke('attr', 'class')
+      .should('contain', 'pagination__item_active');
+    cy.reload();
+    cy.get('[class*="pagination__item"]')
+      .eq(1)
+      .invoke('attr', 'class')
+      .should('contain', 'pagination__item_active');
+  });
+
+  it('Change results list after click pagination page', () => {
+    cy.get('[class*="result__text"]').eq(0).contains('Pomodoro_Timer');
+    cy.get('[class*="pagination__item"]').eq(1).click();
+    cy.get('[class*="result__text"]').eq(0).contains('basic-js');
+  });
+});
+
+describe('Input search work correctly ', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5173/');
+  });
+  it('typing text into input grants correct result', () => {
+    cy.visit('http://localhost:5173/');
+
+    cy.get('input[type="text"]').type('hello');
+    cy.get('input[type="text"]').should('have.value', 'hello');
+
+    cy.intercept('POST', 'https://api.github.com/graphql').as('graphqlRequest');
+    cy.wait('@graphqlRequest');
+    cy.get('[class*="result__text"]').should(($element) => {
+      expect($element).to.contain('hello');
+    });
+  });
+
+  it('Result item contain all information. Click on repository show Card', () => {
+    cy.visit('http://localhost:5173/');
+
+    cy.get('input[type="text"]').type('hello');
+    cy.get('input[type="text"]').should('have.value', 'hello');
+
+    cy.intercept('POST', 'https://api.github.com/graphql').as('graphqlRequest');
+    cy.wait('@graphqlRequest');
+
+    cy.get('[class*="result__text"]').eq(0).contains('Repository');
+    cy.get('[class*="result__text"]').eq(1).contains('Stars');
+    cy.get('[class*="result__text"]').eq(2).contains('Github');
+    cy.get('[class*="result__text"]').eq(3).contains('commit');
+
+    cy.get('[class*="result__text"]').eq(0).click();
+
+    cy.url().should('eq', 'http://localhost:5173/repo_card');
+
+  });
+});
+
+
+describe('Repository card render correctly', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5173/');
+  });
+  it('Repository card contain all information', () => {
+    cy.get('input[type="text"]').type('hello');
+    cy.get('input[type="text"]').should('have.value', 'hello');
+
+    cy.intercept('POST', 'https://api.github.com/graphql').as('graphqlRequest');
+    cy.wait('@graphqlRequest');
+
+    cy.get('[class*="result__text"]').eq(0).click();
+
+    cy.get('[class*="card"]').should('be.visible');
+    cy.get('[class*="card__stars"]').should('be.visible');
+    cy.get('[class*="profile"]').should('be.visible');
+    cy.get('[class*="profile__name"]').should('be.visible');
+    cy.get('[class*="profile__img"]').should('be.visible');
+    cy.get('[class*="description"]').should('be.visible');
+    cy.get('[class*="description__commit"]').should('be.visible');
+    cy.get('[class*="language"]').should('be.visible');
+  });
+
+
 });
